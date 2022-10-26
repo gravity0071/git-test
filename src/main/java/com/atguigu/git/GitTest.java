6b6d184b73699e5de0c624dbeea4dfe64fd39df1
Xiangyu Wan <78680043+gravity0071@users.noreply.github.com>
2022-10-26 21:52:15 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("hot-fix main");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        
+        //test pull--->from github
     }
 }
